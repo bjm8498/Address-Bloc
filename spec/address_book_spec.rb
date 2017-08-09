93dd8c7357a8d74b6659b0decc761012ea7f97e0
Brian Moore <bjm8498@gmail.com>
2017-08-09 13:29:36 -0700

    Checkpoint 6 problem solved

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -112,6 +112,27 @@ RSpec.describe AddressBook do
         end
         
     end
+    
+    describe "#import_from_csv_2" do
+        
+        it "imports the 1st entry" do
+            book.import_from_csv('entries_2.csv')
+            entry_one = book.entries[0]
+            check_entry(entry_one, "Amon", "510-860-5234", "amonisawesome@gmail.com")
+        end
+        
+        it "imports the 2nd entry" do
+            book.import_from_csv('entries_2.csv')
+            entry_two = book.entries[1]
+            check_entry(entry_two, "Brian", "777-777-7777", "brian@blocmail.com")
+        end
+        
+        it "imports the 3rd entry" do
+            book.import_from_csv('entries_2.csv')
+            entry_three = book.entries[2]
+            check_entry(entry_three, "Cyrus", "877-962-3398", "OsirisNile@yahoo.com")
+        end
+    end
 
     describe "#binary_search"
     
